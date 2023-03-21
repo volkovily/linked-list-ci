787eabf9a1b1fd0d2ee81c3ea4e59a7e0445e9d9
Ilya <volkov.ilyya@gmail.com>
2023-03-21 09:39:39 +0100

    fixed typo in test file

--- list.test.js
+++ list.test.js
@@ -1,11 +1,13 @@
-const List = require("./List");
+const List = require("./list");
 
 describe("List", () => {
-  const list = new List();
-  list.append("a");
-  list.append("b");
-  list.append("c");
-  expect(list.get(0)).toBe("a");
-  expect(list.get(1)).toBe("b");
-  expect(list.get(2)).toBe("c");
+  test("should get items at correct index", () => {
+    const list = new List();
+    list.append("a");
+    list.append("b");
+    list.append("c");
+    expect(list.get(0)).toBe("a");
+    expect(list.get(1)).toBe("b");
+    expect(list.get(2)).toBe("c");
+  });
 });
